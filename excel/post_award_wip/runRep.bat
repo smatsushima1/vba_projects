501d90e002bb754587491814d77a399ae5104556
smatsushima1 <smatsushima1@gmail.com>
2022-03-16 21:40:17 -0500

    add batch and vbscript files

---
+++ excel/post_award_wip/runRep.bat
@@ -0,0 +1,5 @@
+
+@echo off
+echo ################################################################################
+echo Running My Post-Award Report...
+cscript "T:\NRFK\C200\Admin\DIVISION 245\10 PAW\_BACKUPS TO POST-AWARD WIPS\runRepVB.vbs" //nologo
---
+++ excel/post_award_wip/runRepAll.bat
@@ -0,0 +1,15 @@
+
+@echo off
+echo ################################################################################
+date /t
+echo RUNNING ALL POST-AWARD WIPS
+
+call "T:\NRFK\C200\Admin\DIVISION 245\10 PAW\_BACKUPS TO POST-AWARD WIPS\runRep.bat"
+
+call "T:\NRFK\C200\Admin\DIVISION 240\1 Code 240 1\2 - WIP 240.1\Post Award WIPs\_BACKUPS TO POST-AWARD WIPS\runRep.bat"
+
+call "T:\NRFK\C200\Admin\DIVISION 240\2 Code 240 2\3 - Post Award WIP\_BACKUPS TO POST-AWARD WIPS\runRep.bat"
+
+call "T:\NRFK\C200\Admin\DIVISION 240\3 Code 240 3\3 - POST AWARD WIP\_BACKUPS TO POST-AWARD WIPS\runRep.bat"
+
+call "T:\NRFK\C200\Admin\DIVISION 235\5 - Post Award WIP\_BACKUPS TO POST-AWARD WIPS\runRep.bat"
